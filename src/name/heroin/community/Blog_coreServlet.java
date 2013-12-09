6af16c004d7462255c776cfb3bf9187c2c1b9bb1
heroInCommunity <heroin62134@gmail.com>
2013-12-09 14:51:42 +0300

    initial commit

---
+++ src/name/heroin/community/Blog_coreServlet.java
@@ -0,0 +1,13 @@
+package name.heroin.community;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Blog_coreServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
