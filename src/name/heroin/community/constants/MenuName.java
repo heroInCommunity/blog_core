ffc50e76e60c255cfe4afc7b6ec9f51a4082b040
heroInCommunity <heroin62134@gmail.com>
2013-12-20 14:28:14 +0300

    Add menus

--- src/name/heroin/community/constants/MenuName.java
+++ src/name/heroin/community/constants/MenuName.java
@@ -4,7 +4,32 @@ public enum MenuName {
 	ADMIN_ADD_PERMISSION("admin/add_permission"),
 	ADMIN_EDIT_PERMISSION("admin/edit_permission"),
 	ADMIN_PERMISSIONS("admin/permissions"),
-	ADMIN_ALL_PERMISSIONS("admin/all_permissions");
+	ADMIN_ALL_PERMISSIONS("admin/all_permissions"),
+	
+	ADMIN_TAGS("admin/tags"),
+	ADMIN_ALL_TAGS("admin/all_tags"),
+	ADMIN_ADD_TAG("admin/add_tag"),
+	ADMIN_EDIT_TAG("admin/edit_tag"),
+	
+	ADMIN_COMMENTS("admin/comments"),
+	ADMIN_ALL_COMMENTS("admin/all_comments"),
+	ADMIN_ADD_COMMENT("admin/add_comment"),
+	ADMIN_EDIT_COMMENT("admin/edit_comment"),
+	
+	ADMIN_ROLES("admin/roles"),
+	ADMIN_ALL_ROLES("admin/all_roles"),
+	ADMIN_ADD_ROLE("admin/add_role"),
+	ADMIN_EDIT_ROLE("admin_role"),
+	
+	ADMIN_USERS("admin/uesrs"),
+	ADMIN_ALL_USERS("admin/all_uesrs"),
+	ADMIN_ADD_USER("admin/add_user"),
+	ADMIN_EDIT_USER("admin/edit_user"),
+	
+	ADMIN_POSTS("admin/posts"),
+	ADMIN_ALL_POSTS("admin/all_posts"),
+	ADMIN_ADD_POST("admin/add_post"),
+	ADMIN_EDIT_POST("admin/edit_post");	
 
 	private final String value;
 	MenuName(String value) {
